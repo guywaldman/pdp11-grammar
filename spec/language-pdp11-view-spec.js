50c0d43ba7859ef3a77973136855ccaffa3f12d3
Guy Waldman <guywald1@gmail.com>
2016-11-01 02:51:39 +0200

    Remove redundant files and refactor regex on comments

--- spec/language-pdp11-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import LanguagePdp11View from '../lib/language-pdp11-view';
-
-describe('LanguagePdp11View', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
